c2a7dcede5308ae205dc56fcb7a29426d1c7a37e
Непочатых Анна <115982874+Anna-Nepochatykh@users.noreply.github.com>
2022-11-15 10:41:16 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 const nav_btn = document.querySelector('.nav__mobile');
 const nav_mobile = document.querySelector('.nav__menu-mobile');
+const body = document.body;
 
-nav_btn.addEventListener('click', function() {
+nav_btn.addEventListener('click', function(event) {
+    event.stopPropagation();
     nav_mobile.classList.toggle('nav__menu-mobile--active');
     nav_btn.classList.toggle('nav__mobile-close');
+    body.classList.toggle('nav__menu-mobile--active');
+})
+
+window.addEventListener('click', function() {
+    if (body.classList.contains('nav__menu-mobile--active')) {
+        nav_mobile.classList.toggle('nav__menu-mobile--active');
+        nav_btn.classList.toggle('nav__mobile-close');
+        body.classList.toggle('nav__menu-mobile--active');
+    }
+})
+
+nav_mobile.addEventListener('click', function(event) {
+    event.stopPropagation();
 })
